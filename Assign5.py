2b1f94d08e71176e4b6328286bbf269e197e8f16
wangx48 <54646000+wangx48@users.noreply.github.com>
2020-11-01 23:43:57 +0800

    Update Assign5.py
    multiclass MLP

--- Assign5.py
+++ Assign5.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[179]:
+# In[31]:
 
 
 import numpy as np
@@ -15,33 +15,41 @@ maxiter = int(sys.argv[3])
 hiddensize = int(sys.argv[4])
 numhidden = int(sys.argv[5])
 
-
+#filename = 'energydata_complete.csv'
 data = pd.read_csv(filename)
 data = data.iloc[:,1:-1]
-
+data2 = (data - data.min()) / (data.max() - data.min())
 def one_hot(x):
     if x<=30:
-        return 1
+        return 0
     elif x<=50:
-        return 2
+        return 1
     elif x<=100:
-        return 3
+        return 2
     else:
-        return 4
+        return 3
+
+def one_hot2(x):
+    if x<100:
+        return int(x/10)
+    else:
+        return 10
     
-data['Appliances'] = data['Appliances'].apply(lambda x:one_hot(x))
+data['Appliances'] = data['Appliances'].apply(lambda x:one_hot2(x))
 data = np.matrix(data)
+data2 = np.matrix(data2)
 
 n = np.shape(data)[0]
 sample = int(0.7*n)
 d = np.shape(data)[1]-1
-k = 4
+k = 11
 
+y=data[:,0]
 trainindex = np.random.choice(n,sample,replace = False)
 testindex = [i for i in range(n) if i not in trainindex]
 
 
-e = np.eye(4)[np.array(y).astype('int64')-1]
+e = np.eye(k)[np.array(y).astype('int64')]
 
 xtrain = data[trainindex,1:]
 xtest = data[testindex,1:] 
@@ -49,22 +57,13 @@ ytrain = e[trainindex,0]
 ytest = e[testindex,0]
 
 
-# In[180]:
 
 
 def relu(x):
-    i = np.shape(x)[0]
-    j = np.shape(x)[1]
-    for k in range(i):
-        for t in range(j):
-            if x[k][t] >=0:
-                pass
-            else:
-                x[k][t]=0
-    return x
+    return np.maximum(x, 0)
 
-
-# In[181]:
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 
 def softmax(x):
@@ -72,23 +71,26 @@ def softmax(x):
     total = np.sum(o)
     return o/total
 
+def get_loss(scores, labels):
+    scores_norm = scores- np.max(scores, axis=1)
+    scores_norm = np.exp(scores_norm)
+    scores_norm = scores_norm / np.sum(scores_norm, axis=1) 
+  
+    true = labels.reshape(-1).argmax()
 
-# In[182]:
+    true_class_scores = scores_norm.max()
+    loss = np.mean(-np.log(true_class_scores))
+    one_hot = np.zeros(scores.shape)
+    one_hot[0][true] = 1.0
+    grad = scores_norm - one_hot
+
+    return loss, grad
+
 
 
 def relug(x):
-    i = np.shape(x)[0]
-    j = np.shape(x)[1]
-    for k in range(i):
-        for t in range(j):
-            if x[k][t] >0:
-                x[k][t]=1
-            else:
-                x[k][t]=0
-    return x
+    return np.maximum(x, 0)
 
-
-# In[183]:
 
 
 def mlp(maxiter,hiddensize,numhidden,eta):
@@ -96,90 +98,107 @@ def mlp(maxiter,hiddensize,numhidden,eta):
     w = []
     #b.append(np.ones((hiddensize,1)))
     #w.append(np.ones((d,hiddensize)))
-    b.append(np.random.rand(hiddensize,1))
-    w.append(np.random.rand(d,hiddensize))
+    b.append(np.ones((1, hiddensize)))
+    w.append(np.ones((d,hiddensize)))
     for l in range(numhidden-1):
-        b.append(np.random.rand(hiddensize,1))
-        w.append(np.random.rand(hiddensize,hiddensize))
-    b.append(np.random.rand(k,1))
-    w.append(np.random.rand(hiddensize,k))
+        b.append(np.ones((1, hiddensize)))
+        w.append(np.ones((hiddensize,hiddensize)))
+    b.append(np.ones((1, k)))
+    w.append(np.ones((hiddensize,k)))
     
     #    b.append(np.ones((hiddensize,1)))
     #    w.append(np.ones((hiddensize,hiddensize)))
     #b.append(np.ones((k,1)))
     #w.append(np.ones((hiddensize,k)))
     
-    t = 0
+    t = 0 
+    ddb = [0 for i in range(numhidden+1)]
+    ddw = [0 for i in range(numhidden+1)]
+    update = 0
+
     while t <maxiter:
 #feed-forward
         i = random.randint(0,sample-1)
         z = []
-        z.append(np.transpose(xtrain[i]))
-        for l in range(numhidden+1):
-            z.append(relu(b[l]+np.transpose(w[l])*z[l]))
-#backpropagation
-        delta = []
-        o = z[-1]
-        o1 = np.ones((4,1))
-        for g in range(4):
-            o1[g] = 1/(np.exp(o[0]-o[g])+np.exp(o[1]-o[g])+np.exp(o[2]-o[g])+np.exp(o[3]-o[g]))
-        delta.append(o1-np.transpose(np.matrix(ytrain[i])))
+        #print(xtrain[i].shape)
+        z.append(xtrain[i]/10)
+        #z.append(xtrain[i])
+        for l in range(numhidden):
+            #print(np.matmul(z[l], w[l]).shape, b[l].shape)
+            out = np.matmul(z[l], w[l]) + b[l]
+            z.append(relu(out))
+        output = np.matmul(z[-1], w[-1]) + b[-1]
+
+#bp
+        loss, grad = get_loss(output, ytrain[i])
+        #print(grad, w[-1].T)
+        db = np.sum(grad, axis=0)
+        dW = np.matmul(z[-1].T, grad)
+        dX = np.matmul(grad, w[-1].T)
+        b[-1] -= db*eta
+        w[-1] -= dW*eta
+        for i in reversed(range(numhidden)):
+            #dX = np.multiply(dX,np.multiply (z[i+1] , 1-z[i+1]) )
+            dX = np.multiply(dX,(z[i + 1] > 0))
+            db = np.sum(dX, axis=0)
+            dW = np.matmul(z[i].T, dX)
+            dX = np.matmul(dX, w[i].T)
+            b[i] -= db*eta
+            w[i] -= dW*eta
+        update +=1
+        #print(len(ddb))
+        if(update == 100):
+            update = 0
+            #eta *= 0.9
+            #print(t, loss)
+            #print(z[0])
+
+        t += 1
         
-        for i in range(numhidden):
 
-            delta.append(np.multiply(w[-(i+1)]*delta[i],relug(z[-i-2])))
-            
-        for j in range(numhidden):
-            w[j] = w[j]-eta*z[j]*np.transpose(delta[-j-1])
-            b[j] = b[j]-eta*delta[-j-1]
-        t+=1
 
-#test
-    tran = xtrain
+#test  np.argmax(scores, axis=1)
     test = np.shape(ytest)[0]
     prd = []
+    correct = 0
     for a in range(test):
-        tran = np.transpose(xtest[a])
-        for i in range(numhidden+1):
-            tran = np.transpose(w[i])*tran+b[i]
+        #tran = xtest[a]
+        tran = xtest[a] /10
+        for i in range(numhidden):
+            tran = np.matmul(tran , w[i]) + b[i]
             tran = relu(tran)
-        yfit = softmax(tran).tolist()
-        prd.append(yfit.index(max(yfit))+1)
-    accuracy = np.sum(pd.DataFrame(data[testindex,0])[0]==prd)/(n-sample)
-
+        out =   np.matmul(tran , w[-1] ) + b[-1]
+        out =   out.reshape(-1)
+        prd = np.argmax(out)
+        label = np.argmax(ytest[a])
+        if(prd <=2):
+            prd = 0
+        elif(prd <=4):
+            prd = 1
+        elif(prd <=9):
+            prd = 2
+        else:
+            prd = 3
+        if(label <=2):
+            label = 0
+        elif(label <=4):
+            label = 1
+        elif(label <=9):
+            label = 2
+        else:
+            label = 3
+        correct += (prd == label)
+    accuracy = correct / float(test)
+    print('bias')
+    print(b)
+    print('weight')
+    print(w)
     return(accuracy)
 
 
 
 
-# In[184]:
+#print(mlp(30000,100,2,1))
 
+print(mlp(maxiter,hiddensize,numhidden,eta))
 
-mlp(100,5,5,0.01)
-
-
-# In[185]:
-
-
-mlp(100,5,10,0.01)
-
-
-# In[186]:
-
-
-mlp(100,10,5,0.01)
-
-
-# In[187]:
-
-
-mlp(100,10,10,0.01)
-
-
-# In[189]:
-
-
-mlp(100,5,5,0.001)
-
-mlp(maxiter,hiddensize,numhidden,eta)
-  
